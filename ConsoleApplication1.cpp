f30e860301c3f6b4ec7efe8a611bb4a4c06723a6
esqweek <115408408+esqweek@users.noreply.github.com>
2022-10-15 15:43:26 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b, c, x1, x2, d;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	d = b * b - 4 * a * c;
+	if (d > 0) {
+		x1 = ((-b) + sqrt(d)) / (2 * a);
+		x2 = ((-b) - sqrt(d)) / (2 * a);
+		cout << "x1 = " << x1 << "\n";
+		cout << "x2 = " << x2 << "\n";
+	}
+	if (d == 0); {
+		x1 = -(b / (2 * a));
+		cout << "x1 = x2 = " << x1 << "\n";
+	}
+	if (d < 0); {
+		cout << "d<0=>Корней нет ";
+	}
+}
